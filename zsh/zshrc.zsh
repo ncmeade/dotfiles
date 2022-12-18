c2d5f04e904a3fda50d1c54da798e85dae5d3e60
Nicholas Meade <nicholas.c.meade@gmail.com>
2022-12-18 17:45:05 -0500

    Add zshrc

---
+++ zsh/zshrc.zsh
@@ -0,0 +1,105 @@
+# Custom prompt
+source ~/.zsh/prompt.zsh
+
+# Load syntax highlighting
+source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
+
+if [[ "$(tput colors)" == "256" ]]; then
+    ZSH_HIGHLIGHT_STYLES[default]=none
+    ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=160
+    ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=037,bold #,standout
+    ZSH_HIGHLIGHT_STYLES[alias]=fg=064,bold
+    ZSH_HIGHLIGHT_STYLES[builtin]=fg=064,bold
+    ZSH_HIGHLIGHT_STYLES[function]=fg=064,bold
+    ZSH_HIGHLIGHT_STYLES[command]=fg=064,bold
+    ZSH_HIGHLIGHT_STYLES[precommand]=fg=064,underline
+    ZSH_HIGHLIGHT_STYLES[commandseparator]=none
+    ZSH_HIGHLIGHT_STYLES[hashed-command]=fg=037
+    ZSH_HIGHLIGHT_STYLES[path]=fg=166,underline
+    ZSH_HIGHLIGHT_STYLES[globbing]=fg=033
+    ZSH_HIGHLIGHT_STYLES[history-expansion]=fg=white,underline
+    ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=125,bold
+    ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=125,bold
+    ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=none
+    ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=136
+    ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=136
+    ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=136
+    ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=136
+    ZSH_HIGHLIGHT_STYLES[assign]=fg=037
+    ZSH_HIGHLIGHT_STYLES[comment]=fg=246
+fi
+
+# Enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# Initialize completion
+autoload -Uz compinit && compinit -i
+zstyle ':completion:*' menu select=4
+zmodload zsh/complist
+# Use vim style navigation keys in menu completion
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+
+# Initialize editing command line
+autoload -U edit-command-line && zle -N edit-command-line
+
+# Enable interactive comments (# on the command line)
+setopt interactivecomments
+
+# Nicer history
+HISTSIZE=1048576
+HISTFILE="$HOME/.zsh_history"
+SAVEHIST=$HISTSIZE
+setopt appendhistory
+setopt incappendhistory
+setopt extendedhistory
+
+# Time to wait for additional characters in a sequence
+KEYTIMEOUT=1 # corresponds to 10ms
+
+# Use vim as the editor
+export EDITOR=vim
+
+# Use vim style line editing in zsh
+bindkey -v
+# Movement
+bindkey -a 'gg' beginning-of-buffer-or-history
+bindkey -a 'G' end-of-buffer-or-history
+# Undo
+bindkey -a 'u' undo
+bindkey -a '^R' redo
+# Edit line
+bindkey -a '^V' edit-command-line
+# Backspace
+bindkey '^?' backward-delete-char
+bindkey '^H' backward-delete-char
+
+# Use incremental search
+bindkey "^R" history-incremental-search-backward
+
+# Disable shell builtins
+disable r
+
+# Avoid issue with GPG:
+# https://www.gnupg.org/(it)/documentation/manuals/gnupg/Common-Problems.html
+export GPG_TTY=$(tty)
+
+# FZF
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Aliases
+source ~/.zsh/aliases.sh
